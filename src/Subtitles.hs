6ae6bdfc3c57447d6a2e2562ac03bec34f27907e
Francesco Magliocca <franciman12@gmail.com>
2015-11-06 15:35:20 +0100

    Modified Subtitles

--- src/Subtitles.hs
+++ src/Subtitles.hs
@@ -1,54 +1,25 @@
-module Subtitles where
+module SubtitleFuncs where
 
 import Text.Subtitles.SRT
 import Control.Monad
+import Data.Text
 
-newtype SubtitleMonad a = SubtitleMonad { runIt :: Subtitles -> a }
+newtype SubtitleFunc a = SubtitleFunc { run :: Subtitles -> a }
+
+instance Functor SubtitleFunc where
+  fmap g (SubtitleFunc f) = SubtitleFunc (g . f)
+
+instance Applicative SubtitleFunc where
+  pure a = SubtitleFunc (const a)
+  SubtitleFunc transformer <*> SubtitleFunc f = SubtitleFuncFunc (\subs -> transformer subs (f subs))
+
+instance Monad SubtitleFunc where
+  sm >>= g = SubtitleFunc (\subs -> runIt ( g (runIt sm subs) ) subs)
+
+noSubs :: SubtitleFunc a -> a
+noSubs sf = run sf []
+
+withSubs :: Subtitles -> SubtitleFunc a -> a
+withSubs sf subs = run sf subs
 
 
-{-
-Let's prove Functors' properties:
-
-1) fmap id == id
- DIM.  fmap id (SubtitleMonad f) = SubtitleMonad (id . f) = SubtitleMonad f = id (SubtitleMonad f)
-
-2) fmap (g . h) == fmap g . fmap h
- DIM. fmap (g . h) (SubtitleMonad f) = SubtitleMonad ( (g . h) . f ) = SubtitleMonad ( g . (h . f) ) = fmap g (SubtitleMonad (h . f)) = fmap g ( fmap h (SubtitleMonad f) ) = (fmap g . fmap h) (SubtitleMonad f)
-
--}
-instance Functor SubtitleMonad where
-  fmap g (SubtitleMonad f) = SubtitleMonad (g . f)
-
-{-
-Let's prove Applicatives' properties:
-
-1) pure id <*> v = v
- DIM. pure id <*> (SubtitleMonad f)  = SubtitleMonad (\subs -> id) <*> (SubtitleMonad f) = SubtitleMonad (\subs' -> ((\subs -> id) subs') (f subs')) = SubtitleMonad (\subs' -> id (f subs')) = SubtitleMonad (\subs' -> f subs') = SubtitleMonad f
-
-2) pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
- DIM. pure (.) <*> SubtitleMonad f <*> SubtitleMonad g <*> SubtitleMonad h = SubtitleMonad (\subs -> (.)) <*> SubtitleMonad f <*> SubtitleMonad g <*> SubtitleMonad h = SubtitleMonad (\subs' -> ((\subs -> (.)) subs') (f subs')) <*> SubtitleMonad g <*> SubtitleMonad h =
-                                                                           = SubtitleMonad (\subs' -> (.) (f subs')) <*> SubtitleMonad g <*> SubtitleMonad h = SubtitleMonad (\subs'' -> ((.) f subs'') (g subs '')) = SubtitleMonad (\subs'' -> f subs'' . g subs'') <*> SubtitleMonad h =
-                                                                           = SubtitleMonad (\subs''' -> (f subs''' . g subs''')  (h subs''')) = SubtitleMonad (\subs''' -> f subs''' (g subs''' (h subs'''))) = SubtitleMonad f <*> ( SubtitleMonad (\subs''' -> (g subs''') (h subs'''))) =
-                                                                           = SubtitleMonad f <*> ( SubtitleMonad g <*> SubtitleMonad h)
-
-3) pure f <*> pure x = pure (f x)
- DIM. pure f <*> pure x = SubtitleMonad (const f) <*> SubtitleMonad (const x) = SubtitleMonad (\subs -> const f subs (const x subs)) = SubtitleMonad (\subs -> f x) = SubtitleMonad (const (f x)) = pure (f x) 
-
-4) u <*> pure y = pure ($ y) <*> u
- DIM. (SubtitleMonad t) <*> pure y = (SubtitleMonad t) <*> (SubtitleMonad (const y)) = SubtitleMonad (\subs -> (t subs) (const y subs)) = SubtitleMonad (\subs -> t subs $ const y subs) = SubtitleMonad (\subs -> ($ (const y subs)) (t subs)) = SubtitleMonad (\subs -> ($ (const y subs)) (t subs)) =
-                                   SubtitleMonad (\subs -> ($ y) (t subs)) = SubtitleMonad (\subs -> (const ($ y) subs) (t subs)) = (SubtitleMonad (const($ y)) <*> (SubtitleMonad t) = pure ($ y) <*> (SubtitleMonad t)
--}
-instance Applicative SubtitleMonad where
-  pure a = SubtitleMonad (const a)
-  SubtitleMonad transformer <*> SubtitleMonad f = SubtitleMonad (\subs -> transformer subs (f subs))
-
--- TODO: Complete Monads' properties proofs
-
-{-
-Let's prove Monads' properties:
-
-1) return a >>= k  =  k a
- DIM. return a >>= k = SubtitleMonad (\subs -> runIt (k (runIt (SubtitleMonad (const a))) subs) subs) = SubtitleMonad (\subs -> runIt (k ((const a) subs)) subs) = SubtitleMonad (\subs -> runIt (k a) subs) = SubtitleMonad (runIt (k a))
--}
-instance Monad SubtitleMonad where
-  sm >>= g = SubtitleMonad (\subs -> runIt ( g (runIt sm subs) ) subs)
